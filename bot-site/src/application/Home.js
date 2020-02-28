36a6808eccc88738ae2be2cabccd98ce482391e8
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2020-02-28 15:03:53 +0000

    Refactored app

---
+++ bot-site/src/application/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <head>
+          <title>Pub Bot</title>
+        </head>
+        <body>
+          <div className="App-Header">
+            <h1>Welcome to the Pub!</h1>
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
+
