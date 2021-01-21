c0b932c88c0abd2a2890302111035580785775f5
Chris Wilsher <52220166+SirCen@users.noreply.github.com>
2021-01-21 21:58:50 +0000

    Added window pop-out
    Added window pop-out for invite after invite button click

--- bot-site/src/application/InviteButton.js
+++ bot-site/src/application/InviteButton.js
@@ -4,12 +4,13 @@ import '../resources/App.css';
 
 
 export default class InviteButton extends Component {
+    handleOnClick(){
+        window.open('https://discordapp.com/api/oauth2/authorize?client_id=608365015610949661&permissions=8&scope=bot','_blank', 'width=500,height=700,left=100,top=100');
+    }
     render() {
         return (
             <div>
-            <a href="https://discordapp.com/api/oauth2/authorize?client_id=608365015610949661&permissions=8&scope=bot">
-                <Button className="inviteBtn">Click here to Invite!</Button>
-            </a>
+            <Button className="inviteBtn" onClick={this.handleOnClick}>Click here to Invite!</Button>
             </div>
         );
     }
