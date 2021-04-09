501b397b932007ba32e160233e3fec3333027d86
Chris Wilsher <52220166+SirCen@users.noreply.github.com>
2021-04-09 10:44:26 +0100

    initial navbar

--- pubbot/src/components/Navbar.js
+++ pubbot/src/components/Navbar.js
@@ -1,56 +1,81 @@
 import React, {useState} from 'react';
-import logo from '../assets/PubBotLogo.png';
+import logo from '../assets/Chris_logo_file.svg';
 
 export default function Navbar() {
     const [isOpen, isClosed] = useState(false);
-    //const logo = require('../assets/PubBotLogo.png')
     return (
-        <>
-        <nav className="flex items-center justify between flex-wrap bg-gray-700 text-gray-300">
-            <div className="container mx-auto flex flex-wrap items-center flex-shrink-0">
-                <div className="w-full relative flex justify-between">
-                    <a className="leading-relaxed inline-block py-2 left"
-                    href="/"><img className="object-contain"src={logo} alt="PubBot Logo" ></img>
-                    </a>
-                    <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" onClick={() => isClosed(!isOpen)}>
-                        <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-                        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                    </button>
+        //         {/* <div
+        //             className={
+        //                 "lg:flex flex-grow items-center" +
+        //                 (isOpen ? " flex" : " hidden")
+        //             }
+        //             id="example-navbar-danger"
+        <div>
+          <nav class="bg-gray-700 shadow-2xl">
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div class="flex items-center justify-between h-16">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0">
+                    <img class="h-8 w-8" src={logo} alt="PubBot Logo" />
+                  </div>
+                  <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">About</a>
+        
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Invite</a>
+        
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Commands</a>
+        
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Support</a>
+                    </div>
+                  </div>
                 </div>
-                <div
-                    className={
-                        "lg:flex flex-grow items-center" +
-                        (isOpen ? " flex" : " hidden")
-                    }
-                    id="example-navbar-danger"
-                >
-                    <ul className="flex flex-col lg:flex-row list-none mr-auto">
-                        <li className="nav-item">
-                            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-                                <i className="fab text-lg leading-lg text-white opacity-75" /> <span className="ml-2">About</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-                                <span className="ml-2">Invite</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-                                <span className="ml-2">Commands</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-                                <span className="ml-2">Support</span>
-                            </a>
-                        </li>
-                    </ul>
+                <div class="hidden md:block">
+                  <div class="ml-4 flex items-center md:ml-6">
+                    <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                    
+                  </div>
                 </div>
+                <div class="-mr-2 flex md:hidden">
+                  {/* <!-- Mobile menu button --> */}
+                  <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" onClick={()=> isClosed(!isOpen)} aria-expanded="false">
+                    {/* <!--
+                      Heroicon name: outline/menu
+        
+                      Menu open: "hidden", Menu closed: "block"
+                    --> */}
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    {/* <!--
+                      Heroicon name: outline/x
+        
+                      Menu open: "block", Menu closed: "hidden"
+                    --> */}
+                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
             </div>
-        </nav>
-        </>
+        
+            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+            <div class={isOpen ? " flex" : " hidden"}  id="mobile-menu">
+              <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">About</a>
+        
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Invite</a>
+        
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Commands</a>
+        
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Support</a>
+        
+              </div>
+            </div>
+          </nav>
+        </div>
         
     );
 }
