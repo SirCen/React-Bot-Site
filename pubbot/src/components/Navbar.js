dbbe89966ad5a5dc2cfa40ee14be1ebac668067c
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2021-04-10 16:07:31 +0100

    Updated navbar to include drop down

--- pubbot/src/components/Navbar.js
+++ pubbot/src/components/Navbar.js
@@ -3,16 +3,11 @@ import logo from '../assets/Chris_logo_file.svg';
 
 export default function Navbar() {
     const [isOpen, isClosed] = useState(false);
+    const [isDropped, notDropped] = useState(false);
     return (
-        //         {/* <div
-        //             className={
-        //                 "lg:flex flex-grow items-center" +
-        //                 (isOpen ? " flex" : " hidden")
-        //             }
-        //             id="example-navbar-danger"
         <div>
-          <nav class="bg-gray-700 shadow-2xl">
-            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <nav class="bg-gray-700  drop-shadow-2xl">
+            <div class="max-w-8xl mx-auto px-2 sm:px-6 lg:px-8">
               <div class="flex items-center justify-between h-16">
                 <div class="flex items-center">
                   <div class="flex-shrink-0">
@@ -20,12 +15,24 @@ export default function Navbar() {
                   </div>
                   <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">About</a>
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium  font-sans">About</a>
         
-                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Invite</a>
-        
-                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Commands</a>
-        
+                      <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium font-sans">Invite</a>
+                      <div>
+                        <div>
+                          <button  type="button" id="command-menu" onClick={()=> notDropped(!isDropped)} class="text-gray-300  hover:text-green-300 px-3 py-2 text-sm font-medium font-sans">Commands</button>
+                          <div className={
+                          (isDropped ? "block " : "hidden ") +
+                          "origin-bottom-left text-center absolute z-10 w-40 rounded-md shadow-lg py-1 bg-gray-700 ring-1 ring-black ring-opacity-5 focus:outline-none"} role="menu" aria-orientation="vertical" aria-labelledby="command-menu">
+                            <a href="/" class="block px-4 py-2 text-m text-gray-300 hover:text-green-300 font-sans font-medium" role="menuitem">Admin</a>
+                            <a href="/" class="block px-4 py-2 text-m text-gray-300 hover:text-green-300 font-sans font-medium" role="menuitem">Fun</a>
+                            <a href="/" class="block px-4 py-2 text-s text-gray-300 hover:text-green-300 font-sans font-medium" role="menuitem">Music</a>
+                            <a href="/" class="block px-4 py-2 text-s text-gray-300 hover:text-green-300 font-sans font-medium" role="menuitem">Translate</a>
+                            <div className="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25" />
+                              <a href="/" class="block px-4 py-2 text-m text-gray-300 hover:text-green-300 font-sans font-medium" role="menuitem">All</a>
+                          </div>
+                        </div>
+                      </div>
                       <a href="/" class="text-gray-300  hover:text-green-300 px-3 py-2 rounded-md text-sm font-medium">Support</a>
                     </div>
                   </div>
@@ -36,9 +43,10 @@ export default function Navbar() {
                     
                   </div>
                 </div>
-                <div class="-mr-2 flex md:hidden">
+                
+                <div class="-mr-2 p-2 flex md:hidden">
                   {/* <!-- Mobile menu button --> */}
-                  <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" onClick={()=> isClosed(!isOpen)} aria-expanded="false">
+                  <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2  rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" onClick={()=> isClosed(!isOpen)} aria-expanded="false">
                     {/* <!--
                       Heroicon name: outline/menu
         
@@ -71,9 +79,12 @@ export default function Navbar() {
                 <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Commands</a>
         
                 <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Support</a>
-        
+
+                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-green-300 block px-3 py-2 rounded-md text-base font-medium">Login</a>
+                    
               </div>
             </div>
+            <div className="h-0 border border-solid border-t-0 border-blueGray-800 opacity-10 shadow-2xl" />
           </nav>
         </div>
         
--- pubbot/src/pages/Home.js
+++ pubbot/src/pages/Home.js
@@ -3,8 +3,8 @@ import '../index.css';
 
 export default function Home() {
     return (
-        <div className="relative flex min-h-screen bg-gray-700">
-            <div>hi</div>
+        <div className="relative flex min-h-screen bg-gray-700 text-white">
+            <div class="p-10">hi, this is coming soon..just enjoy this non-functional website for now :)</div>
         </div>
     );
 }
